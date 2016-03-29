cda48c78a8b1e6c3d00e408ddd4f6ec552d6cd9a
Matthias Jaeger <Matthias.Jaeger@tu-dortmund.de>
2016-03-30 01:46:04 +0200

    Style Guide update, Kommentare in PythonSkript.py verbessert

--- latex-template/PythonSkript.py
+++ latex-template/PythonSkript.py
@@ -1,3 +1,4 @@
+##################################################### Import system libraries ######################################################
 import matplotlib as mpl
 mpl.rcdefaults()
 mpl.rcParams.update(mpl.rc_params_from_file('meine-matplotlibrc'))
@@ -10,8 +11,22 @@ from uncertainties.unumpy import (
     nominal_values as noms,
     std_devs as stds,
 )
-import Regression as reg
+################################################ Finish importing system libraries #################################################
 
+################################################ Adding subfolder to system's path #################################################
+import os, sys, inspect
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+ # use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"python_custom_scripts")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+############################################# Finish adding subfolder to system's path #############################################
+
+##################################################### Import custom libraries ######################################################
 from curve_fit import ucurve_fit
 from table import (
     make_table,
@@ -20,87 +35,109 @@ from table import (
     make_SI,
     write,
 )
+from regression import (
+    reg_linear,
+    reg_quadratic,
+    reg_cubic
+)
+from error_calculation import(
+    MeanError
+)
+################################################ Finish importing custom libraries #################################################
 
-## FREQUENZLY USED CODE
+
+
+
+
+
+
+################################ FREQUENTLY USED CODE ################################
 #
-## IMPORT
+########## IMPORT ##########
 # t, U, U_err = np.genfromtxt('data.txt', unpack=True)
 # t *= 1e-3
-#
-#
-## ERRORS
+
+
+########## ERRORS ##########
 # R_unc = ufloat(R[0],R[2])
 # U = 1e3 * unp.uarray(U, U_err)
-# Rx_mean = np.mean(Rx)       # Mittelwert und syst. Fehler
-# Rx_mean_err = MeanError(noms(Rx))    # Fehler des Mittelwertes
+# Rx_mean = np.mean(Rx)                 # Mittelwert und syst. Fehler
+# Rx_mean_err = MeanError(noms(Rx))     # Fehler des Mittelwertes
 #
-#
-## CURVE FIT
+## Relative Fehler zum späteren Vergleich in der Diskussion
+# RelFehler_G = (G_mess - G_lit) / G_lit
+# RelFehler_B = (B_mess - B_lit) / B_lit
+# write('build/RelFehler_G.tex', make_SI(RelFehler_G*100, r'\percent', figures=1))
+# write('build/RelFehler_B.tex', make_SI(RelFehler_B*100, r'\percent', figures=1))
+
+
+########## CURVE FIT ##########
 # def f(t, a, b, c, d):
 #     return a * np.sin(b * t + c) + d
 #
-# params = ucurve_fit(f, t, U, p0=[1, 1e3, 0, 0])
-# params = ucurve_fit(reg.reg_linear, x, y)         # linearer Fit
-# params = ucurve_fit(reg.reg_quadratic, x, y)      # quadratischer Fit
-# params = ucurve_fit(reg.reg_cubic, x, y)          # kubischer Fit
-# a, b, c, d = params
-# write('build/loesung-a.tex', make_SI(a * 1e-3, r'\kilo\volt', figures=1))     # type in Anz. signifikanter Stellen
-# write('build/loesung-b.tex', make_SI(b * 1e-3, r'\kilo\hertz', figures=1))
-# write('build/loesung-c.tex', make_SI(c,        r'', figures=1))
-# write('build/loesung-d.tex', make_SI(d * 1e-3, r'\kilo\volt', figures=2))
+# params = ucurve_fit(f, t, U, p0=[1, 1e3, 0, 0])   # p0 bezeichnet die Startwerte der zu fittenden Parameter
+# params = ucurve_fit(reg_linear, x, y)             # linearer Fit
+# params = ucurve_fit(reg_quadratic, x, y)          # quadratischer Fit
+# params = ucurve_fit(reg_cubic, x, y)              # kubischer Fit
+# a, b = params
+# write('build/parameter_a.tex', make_SI(a * 1e-3, r'\kilo\volt', figures=1))       # type in Anz. signifikanter Stellen
+# write('build/parameter_b.tex', make_SI(b * 1e-3, r'\kilo\hertz', figures=2))      # type in Anz. signifikanter Stellen
+
+
+########## PLOTTING ##########
+# plt.clf                   # clear actual plot before generating a new one
 #
+## automatically choosing limits with existing array T1
+# t_plot = np.linspace(np.amin(T1), np.amax(T1), 100)
+# plt.xlim(t_plot[0]-1/np.size(T1)*(t_plot[-1]-t_plot[0]), t_plot[-1]+1/np.size(T1)*(t_plot[-1]-t_plot[0]))
 #
-## PLOTTING
+## hard coded limits
 # t_plot = np.linspace(-0.5, 2 * np.pi + 0.5, 1000) * 1e-3
-# plt.clf
-### AUTOMATICLY CHOSING LIMITS WITH EXISTING ARRAY T1
-## t_plot = np.linspace(np.amin(T1), np.amax(T1), 100)
-## plt.xlim(t_plot[0]-1/np.size(T1)*(t_plot[-1]-t_plot[0]), t_plot[-1]+1/np.size(T1)*(t_plot[-1]-t_plot[0]))
 #
+## standard plotting
 # plt.plot(t_plot * 1e3, f(t_plot, *noms(params)) * 1e-3, 'b-', label='Fit')
 # plt.plot(t * 1e3, U * 1e3, 'rx', label='Messdaten')
-## plt.errorbar(B * 1e3, noms(y) * 1e5, fmt='rx', yerr=stds(y) * 1e5, label='Messdaten')
-## plt.xscale('log')    # logarithmische x-Achse
+## plt.errorbar(B * 1e3, noms(y) * 1e5, fmt='rx', yerr=stds(y) * 1e5, label='Messdaten')        # mit Fehlerbalken
+## plt.xscale('log')                                                                            # logarithmische x-Achse
 # plt.xlim(t_plot[0] * 1e3, t_plot[-1] * 1e3)
 # plt.xlabel(r'$t \:/\: \si{\milli\second}$')
 # plt.ylabel(r'$U \:/\: \si{\kilo\volt}$')
 # plt.legend(loc='best')
 # plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-# plt.savefig('build/loesung-plot.pdf')
-#
-#
-## WRITING TABLES
-# t1, t2 = np.array_split(t * 1e3, 2)
-# U1, U2 = np.array_split(U * 1e-3, 2)
-# write('build/loesung-table.tex', make_table([t1, U1, t2, U2], [3, None, 3, None]))  # type in Nachkommastellen
-### ONLY ONE COLUMN IN A TABLE:
+# plt.savefig('build/aufgabenteil_a_plot.pdf')
+
+
+########## WRITING TABLES ##########
+### IF THERE IS ONLY ONE COLUMN IN A TABLE (workaround):
 ## a=np.array([Wert_d[0]])
 ## b=np.array([Rx_mean])
 ## c=np.array([Rx_mean_err])
 ## d=np.array([Lx_mean*1e3])
 ## e=np.array([Lx_mean_err*1e3])
-## write('build/Tabelle_err_d.tex', make_table([a,b,c,d,e],[0, 1, 0, 1, 1]))
-## FULLTABLE
+#
+# write('build/Tabelle_b.tex', make_table([a,b,c,d,e],[0, 1, 0, 1, 1]))     # Jeder fehlerbehaftete Wert bekommt zwei Spalten
 # write('build/Tabelle_b_texformat.tex', make_full_table(
 #     'Messdaten Kapazitätsmessbrücke.',
 #     'table:A2',
 #     'build/Tabelle_b.tex',
 #     [1,2,3,4,5],              # Hier aufpassen: diese Zahlen bezeichnen diejenigen resultierenden Spaltennummern,
-##                              # die Multicolumns sein sollen
+#                               # die Multicolumns sein sollen
 #     ['Wert',
 #     r'$C_2 \:/\: \si{\nano\farad}$',
 #     r'$R_2 \:/\: \si{\ohm}$',
 #     r'$R_3 / R_4$', '$R_x \:/\: \si{\ohm}$',
 #     r'$C_x \:/\: \si{\nano\farad}$']))
+#
+## Aufsplitten von Tabellen, falls sie zu lang sind
+# t1, t2 = np.array_split(t * 1e3, 2)
+# U1, U2 = np.array_split(U * 1e-3, 2)
+# write('build/loesung-table.tex', make_table([t1, U1, t2, U2], [3, None, 3, None]))  # type in Nachkommastellen
+#
+## Verschmelzen von Tabellen (nur Rohdaten, Anzahl der Zeilen muss gleich sein)
+# write('build/Tabelle_b_composed.tex', make_composed_table(['build/Tabelle_b_teil1.tex','build/Tabelle_b_teil2.tex']))
 
-# # Relative Fehler
-# RelFehler_G = (G_mess - G_lit) / G_lit
-# RelFehler_B = (B_mess - B_lit) / B_lit
-# write('build/RelFehler_G.tex', make_SI(RelFehler_G*100, r'\percent', figures=1))
-# write('build/RelFehler_B.tex', make_SI(RelFehler_B*100, r'\percent', figures=1))
-#
-#
-## ARRAY FUNCTIONS
+
+########## ARRAY FUNCTIONS ##########
 # np.arange(2,10)                   # Erzeugt aufwärts zählendes Array von 2 bis 10
 # np.zeros(15)                      # Erzeugt Array mit 15 Nullen
 # np.ones(15)                       # Erzeugt Array mit 15 Einsen
@@ -112,10 +149,11 @@ from table import (
 #
 # a1,a2 = np.array_split(array, 2)  # Array in zwei Hälften teilen
 # np.size(array)                    # Anzahl der Elemente eines Arrays ermitteln
-#
-## ARRAY INDEXING
+
+
+########## ARRAY INDEXING ##########
 # y[n - 1::n]                       # liefert aus einem Array jeden n-ten Wert als Array
-#
-#
-## DIFFERENT STUFF
+
+
+########## DIFFERENT STUFF ##########
 # R = const.physical_constants["molar gas constant"]      # Array of value, unit, error
