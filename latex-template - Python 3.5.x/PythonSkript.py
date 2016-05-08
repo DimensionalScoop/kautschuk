4278d705ad9ff75de2fc607d46d5699d42bfee33
Matthias Jaeger <Matthias.Jaeger@tu-dortmund.de>
2016-05-09 00:25:33 +0200

    Das neue Python Update erzwingt eine Änderung von table.py Zeile 58:
    uncertainties.core.Variable
    statt
    uncertainties.Variable

---
+++ latex-template - Python 3.5.x/PythonSkript.py
@@ -0,0 +1,161 @@
+##################################################### Import system libraries ######################################################
+import matplotlib as mpl
+mpl.rcdefaults()
+mpl.rcParams.update(mpl.rc_params_from_file('meine-matplotlibrc'))
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+################################################ Finish importing system libraries #################################################
+
+################################################ Adding subfolder to system's path #################################################
+import os, sys, inspect
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+ # use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"python_custom_scripts")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+############################################# Finish adding subfolder to system's path #############################################
+
+##################################################### Import custom libraries ######################################################
+from curve_fit import ucurve_fit
+from table import (
+    make_table,
+    make_full_table,
+    make_composed_table,
+    make_SI,
+    write,
+)
+from regression import (
+    reg_linear,
+    reg_quadratic,
+    reg_cubic
+)
+from error_calculation import(
+    MeanError
+)
+################################################ Finish importing custom libraries #################################################
+
+
+
+
+
+
+
+################################ FREQUENTLY USED CODE ################################
+#
+########## IMPORT ##########
+# t, U, U_err = np.genfromtxt('data.txt', unpack=True)
+# t *= 1e-3
+
+
+########## ERRORS ##########
+# R_unc = ufloat(R[0],R[2])
+# ValueWithErrorFromArray = ufloat(np.mean(array), np.std(array))
+# U = 1e3 * unp.uarray(U, U_err)
+# Rx_mean = np.mean(Rx)                 # Mittelwert und syst. Fehler
+# Rx_mean_err = MeanError(noms(Rx))     # Fehler des Mittelwertes
+#
+## Relative Fehler zum späteren Vergleich in der Diskussion
+# RelFehler_G = (G_mess - G_lit) / G_lit
+# RelFehler_B = (B_mess - B_lit) / B_lit
+# write('build/RelFehler_G.tex', make_SI(RelFehler_G*100, r'\percent', figures=1))
+# write('build/RelFehler_B.tex', make_SI(RelFehler_B*100, r'\percent', figures=1))
+
+
+########## CURVE FIT ##########
+# def f(t, a, b, c, d):
+#     return a * np.sin(b * t + c) + d
+#
+# params = ucurve_fit(f, t, U, p0=[1, 1e3, 0, 0])   # p0 bezeichnet die Startwerte der zu fittenden Parameter
+# params = ucurve_fit(reg_linear, x, y)             # linearer Fit
+# params = ucurve_fit(reg_quadratic, x, y)          # quadratischer Fit
+# params = ucurve_fit(reg_cubic, x, y)              # kubischer Fit
+# a, b = params
+# write('build/parameter_a.tex', make_SI(a * 1e-3, r'\kilo\volt', figures=1))       # type in Anz. signifikanter Stellen
+# write('build/parameter_b.tex', make_SI(b * 1e-3, r'\kilo\hertz', figures=2))      # type in Anz. signifikanter Stellen
+
+
+########## PLOTTING ##########
+# plt.clf                   # clear actual plot before generating a new one
+#
+## automatically choosing limits with existing array T1
+# t_plot = np.linspace(np.amin(T1), np.amax(T1), 100)
+# plt.xlim(t_plot[0]-1/np.size(T1)*(t_plot[-1]-t_plot[0]), t_plot[-1]+1/np.size(T1)*(t_plot[-1]-t_plot[0]))
+#
+## hard coded limits
+# t_plot = np.linspace(-0.5, 2 * np.pi + 0.5, 1000) * 1e-3
+#
+## standard plotting
+# plt.plot(t_plot * 1e3, f(t_plot, *noms(params)) * 1e-3, 'b-', label='Fit')
+# plt.plot(t * 1e3, U * 1e3, 'rx', label='Messdaten')
+## plt.errorbar(B * 1e3, noms(y) * 1e5, fmt='rx', yerr=stds(y) * 1e5, label='Messdaten')        # mit Fehlerbalken
+## plt.xscale('log')                                                                            # logarithmische x-Achse
+# plt.xlim(t_plot[0] * 1e3, t_plot[-1] * 1e3)
+# plt.xlabel(r'$t \:/\: \si{\milli\second}$')
+# plt.ylabel(r'$U \:/\: \si{\kilo\volt}$')
+# plt.legend(loc='best')
+# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# plt.savefig('build/aufgabenteil_a_plot.pdf')
+
+
+########## WRITING TABLES ##########
+### IF THERE IS ONLY ONE COLUMN IN A TABLE (workaround):
+## a=np.array([Wert_d[0]])
+## b=np.array([Rx_mean])
+## c=np.array([Rx_mean_err])
+## d=np.array([Lx_mean*1e3])
+## e=np.array([Lx_mean_err*1e3])
+#
+# write('build/Tabelle_b.tex', make_table([a,b,c,d,e],[0, 1, 0, 1, 1]))     # Jeder fehlerbehaftete Wert bekommt zwei Spalten
+# write('build/Tabelle_b_texformat.tex', make_full_table(
+#     'Messdaten Kapazitätsmessbrücke.',
+#     'table:A2',
+#     'build/Tabelle_b.tex',
+#     [1,2,3,4,5],              # Hier aufpassen: diese Zahlen bezeichnen diejenigen resultierenden Spaltennummern,
+#                               # die Multicolumns sein sollen
+#     ['Wert',
+#     r'$C_2 \:/\: \si{\nano\farad}$',
+#     r'$R_2 \:/\: \si{\ohm}$',
+#     r'$R_3 / R_4$', '$R_x \:/\: \si{\ohm}$',
+#     r'$C_x \:/\: \si{\nano\farad}$']))
+#
+## Aufsplitten von Tabellen, falls sie zu lang sind
+# t1, t2 = np.array_split(t * 1e3, 2)
+# U1, U2 = np.array_split(U * 1e-3, 2)
+# write('build/loesung-table.tex', make_table([t1, U1, t2, U2], [3, None, 3, None]))  # type in Nachkommastellen
+#
+## Verschmelzen von Tabellen (nur Rohdaten, Anzahl der Zeilen muss gleich sein)
+# write('build/Tabelle_b_composed.tex', make_composed_table(['build/Tabelle_b_teil1.tex','build/Tabelle_b_teil2.tex']))
+
+
+########## ARRAY FUNCTIONS ##########
+# np.arange(2,10)                   # Erzeugt aufwärts zählendes Array von 2 bis 10
+# np.zeros(15)                      # Erzeugt Array mit 15 Nullen
+# np.ones(15)                       # Erzeugt Array mit 15 Einsen
+#
+# np.amin(array)                    # Liefert den kleinsten Wert innerhalb eines Arrays
+# np.argmin(array)                  # Gibt mir den Index des Minimums eines Arrays zurück
+# np.amax(array)                    # Liefert den größten Wert innerhalb eines Arrays
+# np.argmax(array)                  # Gibt mir den Index des Maximums eines Arrays zurück
+#
+# a1,a2 = np.array_split(array, 2)  # Array in zwei Hälften teilen
+# np.size(array)                    # Anzahl der Elemente eines Arrays ermitteln
+
+
+########## ARRAY INDEXING ##########
+# y[n - 1::n]                       # liefert aus einem Array jeden n-ten Wert als Array
+
+
+########## DIFFERENT STUFF ##########
+# R = const.physical_constants["molar gas constant"]      # Array of value, unit, error
+#                                                         # http://docs.scipy.org/doc/scipy/reference/constants.html
---
+++ latex-template - Python 3.5.x/python_custom_scripts/table.py
@@ -0,0 +1,181 @@
+import itertools
+import codecs
+import uncertainties
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from uncertainties import ufloat
+
+def make_table(columns, figures=None):
+    assert hasattr(columns[0],'__iter__'), "Wenn nur eine Zeile von Daten vorliegt, funktioniert zip nicht mehr; die Elemente von columns müssen Listen sein, auch wenn sie ihrerseits nur ein Element enthalten."
+
+    if figures is None:
+        figures = [None] * len(columns)
+
+    cols = []
+    for column, figure in zip(columns, figures):
+        if np.any(stds(column)):
+            if figure is None:
+                figure = ''
+            col = list(zip(*['{0:.{1:}uf}'.format(x, figure).split('+/-') for x in column]))
+        else:
+            col = list(zip(*[['{0:.{1:}f}'.format(x, figure)] for x in noms(column)]))
+        cols.extend(col)
+
+    max_lens = [max(len(s) for s in col) for col in cols]
+    cols = [['{0:<{1:}}'.format(s, ml) for s in col] for col, ml in zip(cols, max_lens)]
+
+    rows = list(itertools.zip_longest(*cols))
+
+    return (r' \\' + '\n').join([' & '.join(s for s in row if s is not None) for row in rows]) + r' \\'
+
+def make_composed_table(tables):
+    assert isinstance(tables, list), "You need to give a list of filenames to make_composed_table!"
+    Output = ''
+    for filename in tables:
+        with open(filename, 'r') as f:
+            Output += f.read()
+    return Output
+
+def make_SI(num, unit, exp='', figures=None):
+    y = ufloat(0.0, 0) #siunitx mag kein 0 +- 0, deshalb hier der workaround
+    if num == y:
+        return "(0 \pm 0) ~ \si{" + unit + "}"
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num, figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+def write(filename, content):
+    f = codecs.open(filename, "w", "utf-8")
+    if type(content) == uncertainties.core.Variable:
+        content = "\num{" + str(x.n) + " +- " + str(x.s) + "}"
+        f.write(content)
+        if not content.endswith('\n'):
+            f.write('\n')
+        f.close()
+    else:
+        f.write(content)
+        if not content.endswith('\n'):
+            f.write('\n')
+        f.close()
+
+    #
+    # with open(filename, 'w') as f:
+    #     f.write(content)
+    #     if not content.endswith('\n'):
+    #         f.write('\n')
+
+
+def make_full_table(caption,label,source_table, stacking=np.array([]), units=None):
+    # Vorgeplänkel
+    Output = """\\begin{table}
+    \\centering
+    \\caption{""" + caption + """}
+    \\label{""" + label + """}
+    \\sisetup{parse-numbers=false}
+    \\begin{tabular}{\n"""
+
+    # Kerngeschäft : source_table einlesen und verarbeiten, dh. Vor und Nachkommastellen rausfinden
+    counter_columns = 0
+    counter_lines = 0
+    with open(source_table, 'r') as f:
+        Text = f.read()
+        for buchstabe in Text:
+            if (buchstabe == '&'):
+                counter_columns += 1
+            elif (buchstabe == '\\'):
+                counter_lines += 1
+
+    NumberOfLines = counter_lines/2
+    NumberOfColumns = counter_columns/counter_lines*2+1
+    counter_digits_preDot = np.zeros((NumberOfLines, NumberOfColumns), dtype=np.int)
+    counter_digits_postDot = np.zeros((NumberOfLines, NumberOfColumns), dtype=np.int)
+    dot_reached = False
+    counter_columns = 0
+    counter_lines = 0
+    with open(source_table, 'r') as f:
+        Text = f.read()
+    # 'Vor und Nachkommastellen rausfinden' beginnt hier
+        for buchstabe in Text:
+            if (buchstabe == '&'):
+                counter_columns += 1
+                dot_reached = False
+            elif (buchstabe == '.'):
+                dot_reached = True
+            elif (buchstabe == '\\'):
+                counter_lines += 1
+                counter_columns = counter_columns % (NumberOfColumns-1)
+                dot_reached = False
+            elif (buchstabe != ' ') & (buchstabe != '\n'):
+                if (counter_lines/2 <= (NumberOfLines-1)):
+                    if dot_reached == False:
+                        counter_digits_preDot[counter_lines/2][counter_columns] += 1
+                    else:
+                        counter_digits_postDot[counter_lines/2][counter_columns] += 1
+    # jetzt ermittle maximale Anzahl an Stellen und speichere sie in MaxDigitsPreDot und MaxDigitsPostDot
+    MaxDigitsPreDot = []
+    counter_digits_preDot_np = np.array(counter_digits_preDot)
+    for x in counter_digits_preDot_np.T:
+        MaxDigitsPreDot.append(max(x))
+    MaxDigitsPostDot = []
+    counter_digits_postDot_np = np.array(counter_digits_postDot)
+    for x in counter_digits_postDot_np.T:
+        MaxDigitsPostDot.append(max(x))
+    # --------------------Ende der Stellensuche
+
+    # Die Liste stacking in ein angepasstes Array umwandeln mit den tatsächlich betroffenen Spalten
+    stacking_list = np.array(stacking)
+    i = 0
+    for x in stacking_list:
+        stacking_list[i] += i
+        i += 1
+
+    # Schreiben der Tabellenformatierung
+    if np.size(stacking) == 0:
+        for digits_preDot, digits_postDot in zip(MaxDigitsPreDot, MaxDigitsPostDot):
+            Output += '\tS[table-format=' + str(digits_preDot) + '.' + str(digits_postDot) +']\n'
+    else:   # es wurden fehlerbehaftete Werte übergeben, daher muss +- zwischen die entsprechenden Spalten
+        i = 0.0
+        for digits_preDot, digits_postDot in zip(MaxDigitsPreDot, MaxDigitsPostDot):
+            if i in stacking_list:
+                Output += '\tS[table-format=' + str(digits_preDot) + '.' + str(digits_postDot) +']\n'
+                Output += '\t@{${}\\pm{}$}\n'
+            elif i-1 in stacking_list:
+                Output += '\tS[table-format=' + str(digits_preDot) + '.' + str(digits_postDot) +', table-number-alignment = left]\n'      # wir wollen hier linksbündige Zahlen
+            else:
+                Output += '\tS[table-format=' + str(digits_preDot) + '.' + str(digits_postDot) +']\n'
+            i += 1
+
+    # Zwischengeplänkel
+    Output += '\t}\n\t\\toprule\n\t'
+
+    # Einheitenzeile
+    i=0
+    stacking_list = np.array(stacking)
+    for Spaltenkopf in units:
+        if i in stacking_list:
+            Output += '\\multicolumn{2}{c}'
+        Output += '{' + str(Spaltenkopf) + '}\t\t'
+        i += 1
+        if i == np.size(units):
+            Output += '\\\\ \n\t'
+        elif i % 2 == 0:
+            Output += '& \n\t'
+        else:
+            Output += '& '
+
+    # Schlussgeplänkel
+    Output += """\\midrule
+    \\input{""" + source_table + """}
+    \\bottomrule
+    \\end{tabular}
+    \\end{table}"""
+    return Output
